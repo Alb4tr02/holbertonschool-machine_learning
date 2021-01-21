6c38c1a37a6dd0955db4d2463727c7ddac6b2ecd
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-21 18:49:30 -0500

    7-gettin_cozy.py

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""function def cat_matrices2D(mat1, mat2, axis=0) concatenates two matrices"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """INPUT: two matrices
+       OUTPUT: new matrix concatenating the two given matrices
+    """
+    m1 = [row.copy() for row in mat1]
+    m2 = [row.copy() for row in mat2]
+    if (axis == 0 and len(m1[0]) != len(m2[0])
+            or axis == 1 and len(m1[0]) != len(m2)):
+        return None
+    if axis == 0:
+        m1.extend(m2)
+    else:
+        for i in range(len(m1)):
+            m1[i].extend(m2[i])
+    return m1
