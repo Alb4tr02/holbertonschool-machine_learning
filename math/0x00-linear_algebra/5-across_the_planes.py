b118fe5e6f8b8b7401ad6a6fc76260a12fea8209
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-21 14:39:45 -0500

    5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -6,6 +6,8 @@ def add_matrices2D(mat1, mat2):
     """INPUT: two arrays
        OUTPUT: two arrays element-wise added:
     """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
     result_matrix = []
     for i in range(len(mat1)):
         row = [mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))]
