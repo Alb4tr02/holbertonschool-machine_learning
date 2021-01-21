a6921dbfbf7fd76572de5ea028d61fc9afa604ad
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-21 14:52:05 -0500

    6-howdy_partner.py

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function def cat_arrays(arr1, arr2): concatenates two arrays::"""
+
+
+def cat_arrays(arr1, arr2):
+    """INPUT: two arrays
+       OUTPUT: new array with contents of arr1 and arr2
+    """
+    return arr1 + arr2
