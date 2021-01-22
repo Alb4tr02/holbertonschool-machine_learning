877d7fe9520e4baf10bc4292139f60cf3d06b97d
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-21 19:58:42 -0500

    10-ill_use_my_scale.py

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function np_shape(matrix): calculates the shape of a numpy.ndarray"""
+
+def np_shape(matrix):
+    """
+    INPUT: numpy.ndarray
+    OUTPUT: matrix's shape
+    """
+    return matrix.shape
