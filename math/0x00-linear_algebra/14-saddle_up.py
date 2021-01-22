b2c811e0dae8e8e3deb5137c7e14bce961be5cf6
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-21 20:34:06 -0500

    14-saddle_up.py

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function np_matmul(mat1, mat2) performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    INPUT: two matrix
+    OUTPUT: product of matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
