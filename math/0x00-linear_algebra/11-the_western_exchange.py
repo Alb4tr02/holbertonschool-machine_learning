68123f44c38b9f89a4e744079c0b0c8b94ce98ee
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-21 20:04:38 -0500

    11-the_western_exchange.py

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""function def np_transpose(matrix): transposes matrix"""
+
+
+def np_transpose(matrix):
+    """
+    INPUT: matrix
+    OUTPUT: matrix transposed
+    """
+    return matrix.transpose()
