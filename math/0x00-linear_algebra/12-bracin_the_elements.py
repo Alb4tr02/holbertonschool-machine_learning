b078472206822a86a6558f4c2a262a39463a97a9
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-21 20:15:24 -0500

    12-bracin_the_elements.py

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function def np_elementwise(mat1, mat2): performs element-wise operations
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    INPUT: two matrix
+    OUTPUT: element-wise operations between matrices
+    """
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
