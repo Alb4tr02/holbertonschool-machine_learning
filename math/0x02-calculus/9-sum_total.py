55b42ade6e2c92b9810a8aa02d744c3f480a37c6
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-28 19:58:51 -0500

    9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -7,6 +7,6 @@ def summation_i_squared(n):
     INPUT a number
     OUTPUT summation of first n numbers squared
     """
-    if type(n) is not int or n < 0:
+    if type(n) is not int or n <= 0:
         return None
-    return 0 if n == 0 else int(n * (n + 1) * (2 * n + 1) / 6)
+    return int(n * (n + 1) * (2 * n + 1) / 6)
