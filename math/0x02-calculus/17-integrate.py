e93cab2dc421322ecb39c2301b32bc3a25e1b614
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-28 21:46:44 -0500

    17-integrate.py

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Integration """
+
+
+def poly_integral(poly, C=0):
+    """
+    INPUT: polynomial
+    OUTPUT integration polynomial
+    """
+    if type(poly) is not list or len(poly) == 0 or type(C) is not int:
+        return None
+
+    if type(poly[0]) is not int and type(poly[0]) is not float:
+        return None
+    
+    integration = [C] + [poly[i] / (i + 1) for i in range(len(poly))]
+    integration = [int(x) if x % 1 == 0 else x for x in integration]
+    return integration
