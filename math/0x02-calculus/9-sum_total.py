bb2830f52fadb8258bd4e45de8461684c7b0266c
Alb4tr02 <jodmunozol@unal.edu.co>
2021-01-28 19:45:19 -0500

    9-sum_total.py

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Sigma square"""
+
+
+def summation_i_squared(n):
+    """
+    INPUT a number
+    OUTPUT summation of first n numbers squared
+    """
+    if type(n) is not int:
+        return None
+    return 0 if n == 0 else n * n + summation_i_squared(n - 1)
