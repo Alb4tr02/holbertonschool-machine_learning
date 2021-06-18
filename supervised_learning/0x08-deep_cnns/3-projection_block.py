c3ccde1f602fd792422d2039836e2b90cfd12053
tony <jodmunozol@unal.edu.co>
2021-06-17 23:12:57 -0500

    test cases

---
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+This is the projection block.
+"""
+
+import tensorflow.keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+    """This function builds a projection block as described in Deep Residual Learning for
+    Image Recognition (2015).
+
+    Args:
+        A_prev (Keras layer): the output from the previous layer.
+        filters (list or tuple): [F11, F3, F12]
+                                 F11 is the number of filters in the first 1x1
+                                 convolution.
+                                 F3 is the number of filters in the 3x3
+                                 convolution.
+                                 F12 is the number of filters in the second
+                                 1x1 convolution as well as the 1x1
+                                 convolution in the shortcut connection.
+        s (int): stride of the first convolution in both the main path and the
+                 shortcut connection.
+    Returns:
+        The activated output of the identity block.
+    """
+    X = K.layers.Conv2D(filters=filters[0],
+                        kernel_size=1,
+                        padding='same',
+                        strides=(s, s),
+                        kernel_initializer='he_normal')(A_prev)
+
+    X = K.layers.BatchNormalization(axis=3)(X)
+
+    X = K.layers.Activation('relu')(X)
+
+    X = K.layers.Conv2D(filters=filters[1],
+                        kernel_size=3,
+                        padding='same',
+                        # strides=(s, s),
+                        kernel_initializer='he_normal')(X)
+
+    X = K.layers.BatchNormalization()(X)
+
+    X = K.layers.Activation('relu')(X)
+
+    X = K.layers.Conv2D(filters=filters[2],
+                        kernel_size=1,
+                        padding='same',
+                        # strides=(s, s),
+                        kernel_initializer='he_normal')(X)
+
+    X = K.layers.BatchNormalization()(X)
+
+    # Shortcut connection.
+    shortcut = K.layers.Conv2D(filters=filters[2],
+                               kernel_size=1,
+                               padding='same',
+                               strides=(s, s),
+                               kernel_initializer='he_normal')(A_prev)
+
+    shortcut = K.layers.BatchNormalization()(shortcut)
+
+    adding = K.layers.Add()([X, shortcut])
+
+    output = K.layers.Activation('relu')(adding)
+
+    return output
---
+++ supervised_learning/0x08-deep_cnns/README.md
@@ -0,0 +1,1 @@
+# This project is about different deep Convolutional Architectures
